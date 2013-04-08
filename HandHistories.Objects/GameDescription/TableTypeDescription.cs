e18fd08833fcc7eb1d6f90ded351734fc3e620b4
Kader Belbina <kbelbina@predatorwebsystems.com>
2013-04-07 20:29:13 -0400

    Strobe table types

--- HandHistories.Objects/GameDescription/TableTypeDescription.cs
+++ HandHistories.Objects/GameDescription/TableTypeDescription.cs
@@ -19,6 +19,8 @@ namespace HandHistories.Objects.GameDescription
         [EnumMember] PushFold,
         [EnumMember] Any,
         [EnumMember] Zoom,
+        [EnumMember] Strobe,
+        [EnumMember] All,
         [EnumMember] Unknown,
     }
 }
