89ccbd4be9e9e74d211cf34db1ae76a142537ded
ItalyToast <ItalyToast@gmail.com>
2013-09-03 11:27:00 +0200

    Optimized TableTypeDescription and added time measurement in test
    TableTypeDescription is now a bitfield with FlagsAttribute instead of a
    list to milit the memory ysed to 32bits instead of 32/64 + items in the
    list, it makes the parser 5-10% faster using the
    PokerstarsFastImplementation on 1000Hands

--- HandHistories.Objects/GameDescription/TableType.cs
+++ HandHistories.Objects/GameDescription/TableType.cs
@@ -9,18 +9,11 @@ namespace HandHistories.Objects.GameDescription
 {
     [DataContract]
     [Serializable]
-    public class TableType : IEnumerable<TableTypeDescription>
+    public struct TableType
     {
-        [DataMember]
-        private readonly List<TableTypeDescription> _tableTypeDescriptions;
-
-        private TableType(params TableTypeDescription [] tableTypeDescriptions)
-        {
-            // Alphabetize them and take the distincts
-            _tableTypeDescriptions = new List<TableTypeDescription>(tableTypeDescriptions.OrderBy(t => t.ToString()).Distinct());
-        }
-
+        #region Statics
         private static readonly Regex ParseRegex = new Regex("[-,;_]");
+
         public static TableType Parse(string tableType)
         {
             List<string> tableTypeDescriptionStrings = ParseRegex.Split(tableType).ToList();
@@ -33,47 +26,105 @@ namespace HandHistories.Objects.GameDescription
                 );           
         }
 
-        public static TableType FromTableTypeDescriptions(params TableTypeDescription [] tableTypeDescriptions)
+        public static TableType FromTableTypeDescriptions(params TableTypeDescription[] tableTypeDescriptions)
         {
             return new TableType(tableTypeDescriptions);
         }
+        #endregion
 
-        public IEnumerable<TableTypeDescription> GetTableTypeDescriptions()
-        {
-            return _tableTypeDescriptions;
-        }
+        [DataMember]
+        private readonly TableTypeDescription _tableTypeDescriptions;
 
-        public IEnumerator<TableTypeDescription> GetEnumerator()
+        private TableType(params TableTypeDescription [] tableTypeDescriptions)
         {
-            return _tableTypeDescriptions.GetEnumerator();
+            // Alphabetize them and take the distincts
+            _tableTypeDescriptions = 0;
+            foreach (var item in tableTypeDescriptions)
+	        {
+                _tableTypeDescriptions |= item;
+            }
         }
 
         public override string ToString()
         {
-            return string.Join("-", _tableTypeDescriptions);
+            return _tableTypeDescriptions.ToString();
         }
 
-        public override bool Equals(object obj)
+        public List<TableTypeDescription> ToEnumList()
         {
-            TableType tableType = obj as TableType;
-            if (tableType == null)
-            {
-                return false;
-            }
-
-            // See if contains all the same elements
-            //  http://stackoverflow.com/questions/1673347/linq-determine-if-two-sequences-contains-exactly-the-same-elements
-            return new HashSet<TableTypeDescription>(tableType.GetTableTypeDescriptions()).SetEquals(this.GetTableTypeDescriptions());
-        }        
-
-        public override int GetHashCode()
-        {
-            return ToString().GetHashCode();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
+            return _tableTypeDescriptions.ToString()
+                  .Split(new[] { ", " }, StringSplitOptions.None)
+                  .Select(v => (TableTypeDescription)Enum.Parse(typeof(TableTypeDescription), v)).ToList();
         }
     }
+
+    //[DataContract]
+    //[Serializable]
+    //public class TableType : IEnumerable<TableTypeDescription>
+    //{
+    //    [DataMember]
+    //    private readonly List<TableTypeDescription> _tableTypeDescriptions;
+
+    //    private TableType(params TableTypeDescription [] tableTypeDescriptions)
+    //    {
+    //        // Alphabetize them and take the distincts
+    //        _tableTypeDescriptions = new List<TableTypeDescription>(tableTypeDescriptions.OrderBy(t => t.ToString()).Distinct());
+    //    }
+
+    //    private static readonly Regex ParseRegex = new Regex("[-,;_]");
+    //    public static TableType Parse(string tableType)
+    //    {
+    //        List<string> tableTypeDescriptionStrings = ParseRegex.Split(tableType).ToList();
+
+    //        return FromTableTypeDescriptions(
+    //            tableTypeDescriptionStrings
+    //                .Select(t => (TableTypeDescription)Enum.Parse(typeof(TableTypeDescription), t))
+    //                .Distinct()
+    //                .ToArray()
+    //            );           
+    //    }
+
+    //    public static TableType FromTableTypeDescriptions(params TableTypeDescription [] tableTypeDescriptions)
+    //    {
+    //        return new TableType(tableTypeDescriptions);
+    //    }
+
+    //    public IEnumerable<TableTypeDescription> GetTableTypeDescriptions()
+    //    {
+    //        return _tableTypeDescriptions;
+    //    }
+
+    //    public IEnumerator<TableTypeDescription> GetEnumerator()
+    //    {
+    //        return _tableTypeDescriptions.GetEnumerator();
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return string.Join("-", _tableTypeDescriptions);
+    //    }
+
+    //    public override bool Equals(object obj)
+    //    {
+    //        TableType tableType = obj as TableType;
+    //        if (tableType == null)
+    //        {
+    //            return false;
+    //        }
+
+    //        // See if contains all the same elements
+    //        //  http://stackoverflow.com/questions/1673347/linq-determine-if-two-sequences-contains-exactly-the-same-elements
+    //        return new HashSet<TableTypeDescription>(tableType.GetTableTypeDescriptions()).SetEquals(this.GetTableTypeDescriptions());
+    //    }        
+
+    //    public override int GetHashCode()
+    //    {
+    //        return ToString().GetHashCode();
+    //    }
+
+    //    IEnumerator IEnumerable.GetEnumerator()
+    //    {
+    //        return GetEnumerator();
+    //    }
+    //}
 }
