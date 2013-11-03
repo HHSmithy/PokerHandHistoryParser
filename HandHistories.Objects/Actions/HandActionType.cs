9dd099dda6d6991b43269f0a459ffae045368013
ItalyToast <ItalyToast@gmail.com>
2013-11-03 19:56:28 +0100

    Added Ante to Blinds Flag(0x40)

--- HandHistories.Objects/Actions/HandActionType.cs
+++ HandHistories.Objects/Actions/HandActionType.cs
@@ -8,7 +8,6 @@
     public enum HandActionType : byte
     {
         UNKNOWN = 0x0, 
-        ANTE,
         SHOWS_FOR_LOW,
         DEALT_HERO_CARDS,
         UNCALLED_BET,
@@ -43,6 +42,7 @@
         POSTS = 0x40,
         SMALL_BLIND = 0x41,
         BIG_BLIND = 0x42,
+        ANTE = 0x43,
         #endregion
 
         #region ShowDown Action
@@ -51,7 +51,7 @@
         WINS = 0x81,
         WINS_THE_LOW = 0x85,
         WINS_SIDE_POT = 0x89,
-        TIES = 0x82,
+        TIES = 0x87,
         TIES_SIDE_POT = 0x83,
         #endregion
     }
