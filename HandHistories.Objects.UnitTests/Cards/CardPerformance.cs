cdfcaca5cc36c9d8dd983fd43660d73a036cdb79
ItalyToast <ItalyToast@gmail.com>
2014-03-11 01:06:47 +0100

    Changing the Card class to a Struct
    and modified parsing to use lookup tables and enums to get 20x speed
    when parsing hands.
    
    Added Card parsing performance Test.
    
    Varning: Since cards is now a struct they cant be compared to null.

---
+++ HandHistories.Objects.UnitTests/Cards/CardPerformance.cs
@@ -0,0 +1,26 @@
+﻿using HandHistories.Objects.Cards;
+using NUnit.Framework;
+using System;
+
+namespace HandHistories.Objects.UnitTests.Cards
+{
+    class CardPerformanceConstructionTest
+    {
+        [Test]
+        public void NewCard_Performance()
+        {
+            const int numberOfCards = 20000000;
+            System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+            timer.Start();
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                Card card = new Card("T", "h");
+            }
+            timer.Stop();
+            double elapsedMillis = timer.Elapsed.TotalMilliseconds;
+            Console.Write("Time in milliseconds to generate " + numberOfCards + " cards: " + elapsedMillis);
+            bool ExecutionTimeLessThanFiveSeconds = (elapsedMillis < 1000);
+            Assert.IsTrue(ExecutionTimeLessThanFiveSeconds, "Should take less than one seconds: Time Taken: " + elapsedMillis + "ms");
+        }
+    }
+}
