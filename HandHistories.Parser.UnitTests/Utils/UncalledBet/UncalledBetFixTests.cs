faac852a152baba38060226a03af965d86c74306
ItalyToast <ItalyToast@gmail.com>
2015-06-03 13:55:20 +0200

    Refactoring FixUncalledBet to a util
    This is so we can reuse it for every site and makes for much easier
    testing

---
+++ HandHistories.Parser.UnitTests/Utils/UncalledBet/UncalledBetFixTests.cs
@@ -0,0 +1,81 @@
+﻿using HandHistories.Objects.Actions;
+using HandHistories.Objects.Hand;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HandHistories.Parser.Utils.Uncalled;
+using HandHistories.Objects.Cards;
+
+namespace HandHistories.Parser.UnitTests.Utils.Uncalled
+{
+    [TestFixture]
+    class UncalledBetFixTests
+    {
+        void TestUncalledbet(string expectedPlayer, decimal expectedAmount, HandHistory hand)
+        {
+            var actions = UncalledBet.Fix(hand.HandActions, hand.TotalPot, hand.Rake);
+
+            var lastAction = actions.Last();
+
+            Assert.AreEqual(expectedPlayer, lastAction.PlayerName);
+            Assert.AreEqual(expectedAmount, lastAction.Amount);
+        }
+
+        [TestCase]
+        public void UncalledBetTest_UncalledBet_1()
+        {
+            HandHistory hand = new HandHistory();
+
+            hand.HandActions = new List<HandAction>
+            {
+                new HandAction("P1", HandActionType.SMALL_BLIND, 0.1m, Street.Preflop),
+                new HandAction("P2", HandActionType.BIG_BLIND, 0.2m, Street.Preflop),
+                new HandAction("P1", HandActionType.CALL, 0.1m, Street.Preflop),
+                new HandAction("P2", HandActionType.CHECK, 0m, Street.Preflop),
+
+                new HandAction("P1", HandActionType.CHECK, 0m, Street.Flop),
+                new HandAction("P2", HandActionType.BET, 0.1m, Street.Flop),
+                new HandAction("P1", HandActionType.FOLD, 0m, Street.Flop),
+            };
+
+            TestUncalledbet("P2", 0.1m, hand);
+        }
+
+        [TestCase]
+        public void UncalledBetTest_UncalledBB_1()
+        {
+            HandHistory hand = new HandHistory();
+
+            hand.HandActions = new List<HandAction>
+            {
+                new HandAction("SB", HandActionType.SMALL_BLIND, 1m, Street.Preflop),
+                new HandAction("BB", HandActionType.BIG_BLIND, 2m, Street.Preflop),
+            };
+
+            TestUncalledbet("BB", 1m, hand);
+        }
+
+        [TestCase]
+        public void UncalledBetTest_UncalledRaise_1()
+        {
+            HandHistory hand = new HandHistory();
+
+            hand.HandActions = new List<HandAction>
+            {
+                new HandAction("P1", HandActionType.SMALL_BLIND, 0.1m, Street.Preflop),
+                new HandAction("P2", HandActionType.BIG_BLIND, 0.2m, Street.Preflop),
+                new HandAction("P1", HandActionType.CALL, 0.1m, Street.Preflop),
+                new HandAction("P2", HandActionType.CHECK, 0m, Street.Preflop),
+
+                new HandAction("P1", HandActionType.CHECK, 0m, Street.Flop),
+                new HandAction("P2", HandActionType.BET, 0.1m, Street.Flop),
+                new HandAction("P1", HandActionType.RAISE, 0.6m, Street.Flop),
+                new HandAction("P2", HandActionType.FOLD, Street.Flop),
+            };
+
+            TestUncalledbet("P1", 0.5m, hand);
+        }
+    }
+}
---
+++ HandHistories.Parser/Utils/UncalledBet/UncalledBet.cs
@@ -0,0 +1,92 @@
+﻿using HandHistories.Objects.Actions;
+using HandHistories.Objects.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HandHistories.Parser.Utils.Uncalled
+{
+    public static class UncalledBet
+    {
+        /// <summary>
+        /// Adds a Uncalled bet action if there was a raise that was not called at all or only called partially
+        /// </summary>
+        /// <param name="handActions"></param>
+        /// <returns></returns>
+        public static List<HandAction> Fix(List<HandAction> handActions, decimal? totalPot, decimal? rake)
+        {
+            var realActions = handActions.Where(a => a.IsGameAction && !a.IsWinningsAction && a.HandActionType != HandActionType.FOLD).ToList();
+
+            var lastAction = realActions[realActions.Count - 1];
+
+            switch (lastAction.HandActionType)
+            {
+                case HandActionType.RAISE:
+                    handActions.Add(GetUncalledRaise(realActions, lastAction));
+                    break;
+
+                // when the last action before summary is a bet, we need to return that bet to the according player
+                case HandActionType.BET:
+                    handActions.Add(GetUncalledBet(lastAction));
+                    break;
+
+                // when the last action before the summary is the big blind, we need to return the difference between BB and SB
+                case HandActionType.BIG_BLIND:
+                    handActions.Add(GetUncalledBigBlind(realActions, lastAction));
+                    break;
+            }
+
+            return handActions;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="realActions">a list of handactions only containing CALL/BET/RAISE</param>
+        /// <param name="lastAction">the last realAction</param>
+        /// <returns>an Uncalled Bet action</returns>
+        private static HandAction GetUncalledRaise(List<HandAction> realActions, HandAction lastAction)
+        {
+            // amount to return is the amount the raise player invested - the 2nd largest amount invested by a different player
+            var totalInvestedAmount = realActions.Where(a => a.PlayerName.Equals(lastAction.PlayerName)).Sum(a => a.Amount);
+
+            // now we need to get the maximum amount invested by a different player involved in the hand
+            var totalInvestedAmountOtherPlayer = realActions.Where(a => !a.PlayerName.Equals(lastAction.PlayerName)).GroupBy(a => a.PlayerName)
+                                                            .Select(p => new
+                                                            {
+                                                                PlayerName = p.Key,
+                                                                Invested = p.Sum(x => x.Amount)
+                                                            })
+                                                            .Min(x => x.Invested); // money invested is negative, so take the "max" negative value
+
+
+            return new HandAction(lastAction.PlayerName, HandActionType.UNCALLED_BET, totalInvestedAmount - totalInvestedAmountOtherPlayer, Street.Showdown);
+        }
+
+        /// <summary>
+        /// Creates a new handaction with the diffrence between BB and SB
+        /// </summary>
+        /// <param name="realActions">a list of handactions only containing CALL/BET/RAISE</param>
+        /// <param name="lastAction">the last realAction</param>
+        /// <returns>an Uncalled Bet action</returns>
+        private static HandAction GetUncalledBigBlind(List<HandAction> realActions, HandAction lastAction)
+        {
+            // it can actually happen that there was no SB involved
+            var sbAction = realActions.FirstOrDefault(a => a.HandActionType == HandActionType.SMALL_BLIND);
+            var sbAmount = 0m;
+
+            if (sbAction != null)
+            {
+                sbAmount = sbAction.Amount;
+            }
+
+            return new HandAction(lastAction.PlayerName, HandActionType.UNCALLED_BET, lastAction.Amount - sbAmount, Street.Showdown);
+        }
+
+        private static HandAction GetUncalledBet(HandAction lastAction)
+        {
+            return new HandAction(lastAction.PlayerName, HandActionType.UNCALLED_BET, lastAction.Amount, Street.Showdown);
+        }
+    }
+}
