26e00307c267f9bef88c103555c3f20ab5368ab6
koooee <nickkolegraff@gmail.com>
2015-09-25 23:36:55 +0000

    strsplit not stringsplit

--- analysis/pattern-analysis.R
+++ analysis/pattern-analysis.R
@@ -1,4 +1,4 @@
-split_on_comma = function(s) { stringsplit(s, ',') }
+split_on_comma = function(s) { strsplit(s, ',') }
 d = read.csv('/home/ubuntu/action_orderings/action_ordering.csv', header=FALSE, sep='|', stringsAsFactors = FALSE)
 names(d) = c("handid","actionorder","seatnumberorder","amountorder","num_big_blinds_in_amountorder","amount_pct_into_currentpotorder","num_big_blinds_in_currentpotorder")
 d2 = lapply(d[, 2:ncol(d)], split_on_comma)
