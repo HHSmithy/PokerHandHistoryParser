6be0ed3d0d36c8c811c737242d953df6a274dfdf
koooee <nickkolegraff@gmail.com>
2015-09-25 23:52:41 +0000

    split each sequence into individual obs

--- analysis/pattern-analysis.R
+++ analysis/pattern-analysis.R
@@ -1,4 +1,5 @@
 split_on_comma = function(s) { strsplit(s, ',') }
 d = read.csv('/home/ubuntu/action_orderings/action_ordering.csv', header=FALSE, sep='|', stringsAsFactors = FALSE)
 names(d) = c("handid","actionorder","seatnumberorder","amountorder","num_big_blinds_in_amountorder","amount_pct_into_currentpotorder","num_big_blinds_in_currentpotorder")
-d2 = lapply(d[, 2:ncol(d)], split_on_comma)
+d2 = data.frame(lapply(d[, 2:ncol(d)], split_on_comma))
+names(d2) = c("handid","actionorder","seatnumberorder","amountorder","num_big_blinds_in_amountorder","amount_pct_into_currentpotorder","num_big_blinds_in_currentpotorder")
