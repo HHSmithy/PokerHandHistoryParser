918100b4c0aecff83a35d3b256835166febd7b5d
koooee <nickkolegraff@gmail.com>
2015-11-10 14:40:55 -0800

    added sample data in comments

--- Redshift/create_pokerhandhistory.sql
+++ Redshift/create_pokerhandhistory.sql
@@ -1,34 +1,36 @@
+
+
 create table pokerhandhistory (
 
-DateOfHandUtc	VARCHAR(50)
-,HandId		BIGINT
-,DealerButtonPosition	SMALLINT
-,TableName		VARCHAR(50)
-,GameDescription	VARCHAR(200)
-,NumPlayersActive	SMALLINT
-,NumPlayersSeated	SMALLINT
-,Rake			REAL
-,ComumnityCards		VARCHAR(10)
-,TotalPot		REAL
-,PlayerName		VARCHAR(50)
-,HoleCards		VARCHAR(4)
-,StartingStack		REAL
-,SeatNumber		SMALLINT
-,ActionNumber		SMALLINT
-,Amount			REAL
-,HandActionType		VARCHAR(20)
-,Outs			SMALLINT
-,CardOuts		VARCHAR(200)
-,CurrentHandRank	FLOAT8
-,currentPostSize	REAL
-,Street			VARCHAR(20)
-,IsAggressiveAction	VARCHAR(5)
-,IsAllIn		VARCHAR(5)
-,IsAllInAction		VARCHAR(5)
-,IsBlinds		VARCHAR(5)
-,IsGameAction		VARCHAR(5)
-,IsPreFlopRaise		VARCHAR(5)
-,IsRaise		VARCHAR(5)
-,IsWinningsAction	VARCHAR(5)
+DateOfHandUtc	VARCHAR(50) -- 1/1/2015 10:00:06 AM
+,HandId		BIGINT -- 349921475
+,DealerButtonPosition	SMALLINT -- 4
+,TableName		VARCHAR(50) -- Provo
+,GameDescription	VARCHAR(200) -- Pacific.NoLimitHoldem.$3-$6.Regular.6 Max
+,NumPlayersActive	SMALLINT -- 6
+,NumPlayersSeated	SMALLINT -- 6
+,Rake			REAL -- 4.00
+,ComumnityCards		VARCHAR(10) -- 5s2cTc2s7h
+,TotalPot		REAL -- 
+,PlayerName		VARCHAR(50) -- Beardal1ty
+,HoleCards		VARCHAR(4) -- 
+,StartingStack		REAL -- 927.12
+,SeatNumber		SMALLINT -- 4
+,ActionNumber		SMALLINT -- 6
+,Amount			REAL -- 0
+,HandActionType		VARCHAR(20) -- FOLD
+,Outs			SMALLINT -- 0
+,CardOuts		VARCHAR(200) -- 
+,CurrentHandRank	VARCHAR(25) -- 0
+,currentPostSize	REAL -- -24.75
+,Street			VARCHAR(20) -- Preflop
+,IsAggressiveAction	VARCHAR(5) -- False
+,IsAllIn		VARCHAR(5) -- False
+,IsAllInAction		VARCHAR(5) -- False
+,IsBlinds		VARCHAR(5) -- False
+,IsGameAction		VARCHAR(5) -- True
+,IsPreFlopRaise		VARCHAR(5) -- False
+,IsRaise		VARCHAR(5) -- False
+,IsWinningsAction	VARCHAR(6) -- False
 
 );
