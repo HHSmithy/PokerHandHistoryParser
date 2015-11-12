5d5dc090effa35c7164b001920a0e6091161c05c
koooee <nickkolegraff@gmail.com>
2015-11-12 07:13:37 +0000

    fixed lapply and equal parm length for em.control

--- analysis/working.R
+++ analysis/working.R
@@ -1,5 +1,5 @@
-if(!exists('AK') AK = sthmm(hand_list=c('AKo'))
-if(!exists('AKr') AKr = sthmm(hand_list=c('AKo', '27o', '55o', '5Ao'), model=list(V2~V5, V2~V6, V2~V8), numstates=2, fam=list(multinomial(), multinomial(), multinomial()))
+if(!exists('AK')) AK = sthmm(hand_list=c('AKo'))
+if(!exists('AKr')) AKr = sthmm(hand_list=c('AKo', '27o', '55o', '5Ao'), model=list(V2~V5, V2~V6, V2~V8), numstates=2, fam=list(multinomial(), multinomial(), multinomial()))
 
 # blendHMM = function(
 # 	fittedHmmToBlend
@@ -17,16 +17,17 @@ if(!exists('AKr') AKr = sthmm(hand_list=c('AKo', '27o', '55o', '5Ao'), model=lis
 
 pars=getpars(AKr$fit.dmm)
 AKpars = pars[seq(10,npar(AKr$fit.dmm),4)]
-skipnum = (fittedHmmToBlend@nresp * fittedHmmToBlend@nstates) + 1
-responseinits = pars[skipnum:npar(fittedHmmToBlend)]
+skipnum = (AKr$fit.dmm@nresp * AKr$fit.dmm@nstates) + 1
+responseinits = pars[skipnum:npar(AKr$fit.dmm)]
 helper = function(x, y) { return(c(x, runif(y))) }
+responseinitsfinal = unlist(lapply(AKpars, function(z) helper(z,9)))
 
-blendHMM(
-  hand_list = c('AKo', sample(unique(pdata$V2), 9))
-  , fittedHmmToBlend=AKr$fit.dmm
+AKf = blendHMM(
+  fittedHmmToBlend=AKr$fit.dmm
+  , hand_list = c('AKo', as.character(sample(unique(pdata$V2), 9)))
   , model=list(V2~V5, V2~V6, V2~V8)
   # State 2 is what we want to fix since in AKr model above is was accurate on AKo
-  , startresp = unlist(AKpars, lapply(function(z) helper(z,9)))
+  , startresp = exp(responseinitsfinal) / (1+exp(responseinitsfinal))
   , starttr = c(.5,.5,1,0)
   , startinit = pars[1:2]
-  , fix=c(rep(1, skipnum), rep(1,224), rep(0, 224)))
+  , fix=c(rep(1, skipnum-1), rep(1,14*10), rep(0, 14*10)))
