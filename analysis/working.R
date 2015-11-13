d5875c8e6082f42f9b555bba144e81fe5e18d62d
koooee <nickkolegraff@gmail.com>
2015-11-13 05:28:03 +0000

    try a simpler model

--- analysis/working.R
+++ analysis/working.R
@@ -19,15 +19,31 @@ pars=getpars(AKr$fit.dmm)
 AKpars = pars[seq(10,npar(AKr$fit.dmm),4)]
 skipnum = (AKr$fit.dmm@nresp * AKr$fit.dmm@nstates) + 1
 responseinits = pars[skipnum:npar(AKr$fit.dmm)]
-helper = function(x, y) { return(c(x, runif(y))) }
-responseinitsfinal = unlist(lapply(AKpars, function(z) helper(z,9)))
+hands= c('AKo', as.character(sample(unique(pdata$V2), 5)))
+idx = which('AKo' == sort(hands))
+helper = function(x, y) { return(c(rep(runif(1), idx-1), x, rep(runif(1), length(hands)-idx))) }
+responseinitsfinal = unlist(lapply(AKpars, function(z) helper(z,length(hands))))
 
+
+if(!exists('AKf')){
 AKf = blendHMM(
   fittedHmmToBlend=AKr$fit.dmm
-  , hand_list = c('AKo', as.character(sample(unique(pdata$V2), 9)))
+  , hand_list = hands
   , model=list(V2~V5, V2~V6, V2~V8)
   # State 2 is what we want to fix since in AKr model above is was accurate on AKo
   , startresp = exp(responseinitsfinal) / (1+exp(responseinitsfinal))
-  , starttr = c(.5,.5,1,0)
+  , starttr = pars[3:6]
   , startinit = pars[1:2]
-  , fix=c(rep(1, skipnum-1), rep(1,14*10), rep(0, 14*10)))
+  , fix=c(rep(1, skipnum-1), rep(1,14*length(hands)), rep(0, 14*length(hands))))
+}
+
+print("AK15")
+AK5 = blendHMM(
+  fittedHmmToBlend=AKr$fit.dmm
+  , hand_list = hands
+  , model=list(V5~1, V6~1, V8~1)
+  # State 2 is what we want to fix since in AKr model above is was accurate on AKo
+  , startresp = exp(AKpars) / (1+exp(AKpars))
+  , starttr = c(.5, .5, 0.95, 0.05)
+  , startinit = pars[1:2]
+  , fix=c(rep(1, skipnum-1), rep(1,14), rep(0, 14)))
