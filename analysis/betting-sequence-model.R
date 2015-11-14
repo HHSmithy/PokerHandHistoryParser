f0b833a49b28a9cf5da1319a1f12f1130dd80448
koooee <nickkolegraff@gmail.com>
2015-11-14 10:30:08 +0000

    refactor

--- analysis/betting-sequence-model.R
+++ analysis/betting-sequence-model.R
@@ -55,82 +55,6 @@ names(r.object) = c('eval.dataset', 'iseq', 'dmm', 'fit.dmm', 'eval.matrix')
 return(r.object)
 }
 
-if(!exists('AK')) {
-# Make a sample dataset with subset of hands for a 2 state HMM.
-# Patterns that "look like" player is holding AK or they are NOT holding AK
-# pdataAK = pdata[pdata$V2 %in% c('AKo'), ]
-# pdataAK = pdataAK[order(pdataAK$V1, pdataAK$V4, pdataAK$V7), ]
-AK = stss(c('AKo'))
-}
-# > head(pdataAK)
-#                                               V1  V2         V3
-# 512050  000036366e5344b5b24baa44b989aea35d800578 4Ao 8cKs9d9sTc
-# 1134393 000036366e5344b5b24baa44b989aea35d800578 4Ao 8cKs9d9sTc
-# 237601  000036366e5344b5b24baa44b989aea35d800578 4Ao 8cKs9d9sTc
-# 1794271 000036366e5344b5b24baa44b989aea35d800578 4Ao 8cKs9d9sTc
-# 1062592 000036366e5344b5b24baa44b989aea35d800578 4Ao 8cKs9d9sTc
-# 1990632 000036366e5344b5b24baa44b989aea35d800578 8Qo 8cKs9d9sTc
-#                                               V4          V5      V6 V7    V8
-# 512050  7a10747e52a7e1c2ae191ab84c8eaf9c2e24110f   BIG_BLIND Preflop  2 SMALL
-# 1134393 7a10747e52a7e1c2ae191ab84c8eaf9c2e24110f        CALL Preflop  4 SMALL
-# 237601  7a10747e52a7e1c2ae191ab84c8eaf9c2e24110f       CHECK    Flop  5 SMALL
-# 1794271 7a10747e52a7e1c2ae191ab84c8eaf9c2e24110f       CHECK    Turn  7 SMALL
-# 1062592 7a10747e52a7e1c2ae191ab84c8eaf9c2e24110f       CHECK   River  9 SMALL
-# 1990632 f72089770df258345acc3412c14205fc60586507 SMALL_BLIND Preflop  1 SMALL
-
-# Let the model know we have multiple independant sequences in this dataset
-# we treat each players sequence as independant, this isn't the greatest assumption, however,
-# for recurring behavior this is OK to make.
-# if(!exists('iseq')) {
-# iseq = aggregate(V2 ~ V1 + V4, pdataAK, length)
-# iseq = iseq[order(iseq$V1, iseq$V4), ]
-# }
-
-# if(!exists('iseq_all')){
-# print("Aggregating ..")
-# iseq_all = aggregate(V2 ~ V1 + V4, pdata, length)
-# print("Sorting..")
-# iseq_all = iseq_all[order(iseq_all$V1, iseq_all$V4), ]
-# }
-
-# create a multivariate HMM -- 2 states since we only care about AKo .. either you got it or you don't.
-# if(!exists('dmmR')) { dmmR = depmix(list(V2~V5, V2~V6, V2~V8), data=pdataAK, nstates=2,
-# 		      	     ntimes=iseq[,3],
-# 			     family = list(multinomial(), multinomial(), multinomial())) }
-
-
-# dmmR3 = depmix(list(V2~V5, V2~V6, V2~V8)
-#                , data=pdataAK
-# 	       , nstates=2
-# 	       ,family = list(multinomial(), multinomial(), multinomial()))
-
-# fit.dmmR3 = fit(dmmR3)
-
-# if(!exists('dmmR2')) {
-# dmmR2 = depmix(list(V5~1, V6~1, V8~1)
-# , data=pdataAK
-# , nstates=1
-# , ntimes=iseq[,3]
-# , family = list(multinomial(), multinomial(), multinomial()))
-
-# fit.dmmR2 = fit(dmmR2)
-# }
-
-# Optimize the HMM parameters
-if(!exists('fit.dmmR')) { fit.dmmR = fit(dmmR) }
-
-# Use the parameters from previous training to see how the model holds when we add more hands.
-# pdatasampled = pdata[pdata$V2 %in% c('AKo', '8Ao', 'JTs'), ]
-# pdatasampled = pdatasampled[order(pdatasampled$V1, pdatasampled$V4, pdatasampled$V7), ]
-# pars = getpars(fit.dmmR2)
-# responseinits = pars[7:npar(fit.dmmR2)]
-# flags = c(unlist(getpars(fit.dmmR2, "fixed")))
-# filter = flags[7:npar(fit.dmmR2)]
-# responseinits_filter = responseinits[!filter]
-# helper = function(x) { return(c(x, runif(2))) }
-# responseinitsfinal = c(unlist(lapply(responseinits_filter, helper)))
-# names(responseinitsfinal) = NULL
-
 # Leverage parameters from one fitted HMM model to optimize another HMM model
 blendHMM = function(
 	fittedHmmToBlend
@@ -147,7 +71,7 @@ blendHMM = function(
 	, fixedhand='AKo'
 	, numhands=3
 	, fix=NULL) {
-	  	    print("Creating Validation Data..")
+	  	print("Creating Validation Data..")
 
 		temp = pdata[pdata$V1 %in% seqids, ]
 
